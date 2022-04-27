ffb73fb597eacac44dc04626a4835e17a7e8e68f
Rafael Ahrons <rafael.ahrons@gmail.com>
2022-04-27 10:40:15 -0300

    Add TopicoDTO

--- Alura/forum/src/main/java/br/com/firstproject/forum/controller/TopicosController.java
+++ Alura/forum/src/main/java/br/com/firstproject/forum/controller/TopicosController.java
@@ -1,22 +1,23 @@
 package br.com.firstproject.forum.controller;
 
+import br.com.firstproject.forum.controller.dto.TopicoDTO;
 import br.com.firstproject.forum.modelo.Curso;
 import br.com.firstproject.forum.modelo.Topico;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Arrays;
 import java.util.List;
 
-@Controller
+@RestController
 public class TopicosController {
 
     @RequestMapping("/topicos")
-    @ResponseBody
-    public List<Topico> lista(){
-        Topico topico = new Topico("Duvida", "Duvida com Spring", new Curso("Spring", "Programação"));
+    public List<TopicoDTO> lista(){
+        Topico topico = new Topico("Duvida", "Duvida com ", new Curso("Spring", "Programação"));
 
-        return Arrays.asList(topico);
+        return TopicoDTO.converter(Arrays.asList(topico, topico, topico));
     }
 }
