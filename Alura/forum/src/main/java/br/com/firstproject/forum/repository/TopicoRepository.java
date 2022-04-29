da5996a3ed8ac0ce3ea1857605044ef34106e7dc
Rafael Ahrons <rafael.ahrons@gmail.com>
2022-04-29 20:01:54 -0300

    Add Query annotation and fixed CursoNome method name

--- Alura/forum/src/main/java/br/com/firstproject/forum/repository/TopicoRepository.java
+++ Alura/forum/src/main/java/br/com/firstproject/forum/repository/TopicoRepository.java
@@ -2,6 +2,14 @@ package br.com.firstproject.forum.repository;
 
 import br.com.firstproject.forum.modelo.Topico;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 public interface TopicoRepository extends JpaRepository<Topico, Long> {
+    List<Topico> findByCursoNome(String nomeCurso);
+
+    @Query("SELECT t FROM Topico WHERE t.curso.nome = :nomeCurso")
+    List<Topico> carregarPorNomeDoCurso(@Param("nomeCurso") String nomeCurso);
 }
