30f348b79259b46412f39ab45e0e6a4f39eb91da
Rafael Ahrons <rafael.ahrons@gmail.com>
2022-05-02 11:29:57 -0300

    Fix query from topicoRepository

--- Alura/forum/src/main/java/br/com/firstproject/forum/repository/TopicoRepository.java
+++ Alura/forum/src/main/java/br/com/firstproject/forum/repository/TopicoRepository.java
@@ -10,6 +10,6 @@ import java.util.List;
 public interface TopicoRepository extends JpaRepository<Topico, Long> {
     List<Topico> findByCursoNome(String nomeCurso);
 
-    @Query("SELECT t FROM Topico WHERE t.curso.nome = :nomeCurso")
+    @Query("SELECT t FROM Topico t WHERE t.curso.nome = :nomeCurso")
     List<Topico> carregarPorNomeDoCurso(@Param("nomeCurso") String nomeCurso);
 }
